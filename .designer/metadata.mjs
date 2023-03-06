d547c0c89286fc6ede83cf8ace20e64be5a0f5cb
turrsis <turrsis@gmail.com>
2023-03-06 11:59:53 +0200

    fix tests

--- .designer/metadata.mjs
+++ .designer/metadata.mjs
@@ -1,9 +1,11 @@
+import metadataTests from 'vdsg-metadata-tests'
 import metadataConfigs from 'vdsg-metadata-configs'
 import metadataVuetify from 'vdsg-metadata-vuetify'
 
 //import templates from './metadata.templates.mjs'
 
 export default [
+    metadataTests,
     metadataConfigs,
     metadataVuetify,
     /*{
